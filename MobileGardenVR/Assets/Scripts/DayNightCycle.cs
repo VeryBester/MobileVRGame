63bcef2703060a2e5d043074d956879528b1cd2b
VeryBester <david15202@gmail.com>
2019-04-11 17:24:26 -0500

    day night

---
+++ MobileGardenVR/Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    public Material[] skyboxes = new Material[10];
+    
+    int index;
+    void Start()
+    {
+        index = 0;
+        RenderSettings.skybox = skyboxes[index];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            index++;
+            if(index > skyboxes.Length - 1){
+                index = 0;
+            }
+            RenderSettings.skybox = skyboxes[index];
+            Debug.Log("working" + index);
+        }
+    }
+}
