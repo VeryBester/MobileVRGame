b825ca73b76a22e6b661c959e7a3c0a35179c5bd
VeryBester <david15202@gmail.com>
2019-04-15 01:12:34 -0500

    added timer to day-night cycle

--- MobileGardenVR/Assets/Scripts/DayNightCycle.cs
+++ MobileGardenVR/Assets/Scripts/DayNightCycle.cs
@@ -4,25 +4,39 @@ using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour
 {
-    public Material[] skyboxes = new Material[10];
+    public Material[] skyboxes = new Material[5];
     
-    int index;
     void Start()
     {
-        index = 0;
-        RenderSettings.skybox = skyboxes[index];
+        StartCoroutine(startDay());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Space)){
-            index++;
-            if(index > skyboxes.Length - 1){
-                index = 0;
-            }
-            RenderSettings.skybox = skyboxes[index];
-            Debug.Log("working" + index);
-        }
+    IEnumerator startDay(){
+        RenderSettings.skybox = skyboxes[0];
+        yield return new WaitForSeconds(240);
+        yield return StartCoroutine(startDayBreak());
+
+    }
+    IEnumerator startDayBreak(){
+        RenderSettings.skybox = skyboxes[1];
+        yield return new WaitForSeconds(60);
+        yield return StartCoroutine(startNight());
+    }
+    IEnumerator startNight(){
+        RenderSettings.skybox = skyboxes[2];
+        yield return new WaitForSeconds(240);
+        yield return StartCoroutine(startSunRise1());
+    }
+
+    IEnumerator startSunRise1(){
+        RenderSettings.skybox = skyboxes[3];
+        yield return new WaitForSeconds(60);
+        yield return StartCoroutine(startSunRise2());
+    }
+
+    IEnumerator startSunRise2(){
+        RenderSettings.skybox = skyboxes[4];
+        yield return new WaitForSeconds(60);
+        yield return StartCoroutine(startDay());
     }
 }
