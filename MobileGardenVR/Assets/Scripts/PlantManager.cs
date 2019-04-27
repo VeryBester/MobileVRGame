74eca4225f52792b95a6085a6b9f468be18463c2
ColeFortson <colemanfortson@192.168.1.11>
2019-04-26 21:58:18 -0500

    init plant manager

---
+++ MobileGardenVR/Assets/Scripts/PlantManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant
+{
+    public string type;
+    public GameObject[] stages;
+    public int growthTime;
+    public GameObject dropFruit;
+
+    void Init(string type, GameObject[] stages, int growthTime, GameObject dropFruit)
+    {
+        this.type = type;
+        this.stages = stages;
+        this.growthTime = growthTime;
+        this.dropFruit = dropFruit;
+
+    }
+}
+
+public class PlantManager : MonoBehaviour
+{
+
+    public string type; // Type ID of plant
+    public int stage; 
+    public Plant plant;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
