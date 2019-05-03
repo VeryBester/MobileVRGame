9a928e9a6bff1a5a757c90c4fffab0ec2a7077a4
ColeFortson <colemanfortson@192.168.1.11>
2019-05-03 01:46:24 -0500

    Added ring UI to main scene

---
+++ MobileGardenVR/Assets/Scripts/UIManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+
+    public Image bgRing;
+    public Image greenRing;
+    public Text scoreText;
+
+    private int score;
+    private int maxScore = 100;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setScore(int score)
+    {
+        this.score = score;
+        refreshDisplay();
+    }
+
+    public void refreshDisplay()
+    {
+        greenRing.fillAmount = score / maxScore;
+        scoreText.text = score.ToString();
+    }
+}
